3f72f5939affa5637e2e54195f6b1189800fa97f
Mikael Nilsson <Micke.N90@hotmail.com>
2022-05-03 10:10:26 +0200

    Reroute

--- BackAlleyCinema/Pages/Cinema/Index.cshtml.cs
+++ BackAlleyCinema/Pages/Cinema/Index.cshtml.cs
@@ -8,16 +8,16 @@ namespace BackAlleyCinema.Pages.Cinema
 {
     [BindProperties(SupportsGet = true)]
     public class IndexModel : PageModel
-    {        
+    {
         public Saloon ChosenSaloon { get; set; }
-        
-        public Movie ChosenMovie { get; set; }       
+
+        public Movie ChosenMovie { get; set; }
         public Schedule ThisSchedule { get; set; }
         private CinemaDbContext _context { get; }
         public int Seat { get; set; }
         public int Price { get; set; }
         public int MovieId { get; set; }
-        public int SaloonNr { get; set; }
+        public int SaloonId { get; set; }
         public string ViewsId { get; set; }
 
         public IActionResult OnGet(Dictionary<string, string> data)
@@ -28,7 +28,7 @@ namespace BackAlleyCinema.Pages.Cinema
                 .Where(x => x.Saloons.Id == ChosenSaloon.Id &&
                 x.MovieId == ChosenMovie.Id &&
                 x.ViewsId == DateTime.Parse(data["Time"])).FirstOrDefault();
-            
+
             return Page();
         }
 
@@ -38,19 +38,21 @@ namespace BackAlleyCinema.Pages.Cinema
         }
         public IActionResult OnPost()
         {
+            var saloon = _context.Saloons.Where(x => x.Id == SaloonId).FirstOrDefault();
 
             for (int i = Seat; i < (Price / 100) + Seat; i++)
             {
-                if (ThisSchedule.TakenSeats != null && ThisSchedule.TakenSeats.Contains(Seat.ToString()))
+                if (ThisSchedule.TakenSeats != null && ThisSchedule.TakenSeats.Contains(Seat.ToString()) || i > saloon.MaxSeats - 1)
                 {
 
-                    return Page();
+
+                    return RedirectToPage("/Cinema/Index", new Dictionary<string, string> { { "Movie", MovieId.ToString() }, { "Saloon", SaloonId.ToString() }, { "Time", ViewsId.ToString() }, { "Price", Price.ToString() }, { "FirstSeat", Seat.ToString() } });
                 }
 
             }
 
-            return RedirectToPage("../Payment/Index", new Dictionary<string, string> { { "Movie", MovieId.ToString() }, { "Saloon", SaloonNr.ToString() }, { "Time", ViewsId.ToString() }, { "Price", Price.ToString() }, { "FirstSeat", Seat.ToString() } });
+            return RedirectToPage("../Payment/Index", new Dictionary<string, string> { { "Movie", MovieId.ToString() }, { "Saloon", SaloonId.ToString() }, { "Time", ViewsId.ToString() }, { "Price", Price.ToString() }, { "FirstSeat", Seat.ToString() } });
         }
-        
+
     }
 }
